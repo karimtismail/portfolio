b0ae06b1f38991ec4cdbdf9d9dc995f6d12dbd06
Karim Taha <karim.taha@intercom.com.eg>
2024-09-15 12:48:43 +0300

    init profile

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = '/karimtimsail'
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
-    distDir: 'dist',
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 };
 
 module.exports = nextConfig;
