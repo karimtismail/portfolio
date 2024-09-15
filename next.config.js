a5e499394c468bc202a21b61a923c57c86db412f
Karim Taha <karim.taha@intercom.com.eg>
2024-09-15 13:12:25 +0300

    set basePath

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
-    }
+    },
+    basePath: isProd ? '/portfolio' : '',
 };
 
 module.exports = nextConfig;
